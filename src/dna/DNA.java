5e52692077b1e8c4e0446668028f73504bcf6be6
yoonha <yoonhal@student.ubc.ca>
2022-09-21 13:40:20 -0700

    finished lab

--- src/dna/DNA.java
+++ src/dna/DNA.java
@@ -1,48 +1,45 @@
 package dna;
-import java.lang.String;
 import java.util.HashSet;
-
-// TODO: Implement the DNA datatype from scratch!
-// Use the test cases to guide you.
 
 public class DNA {
     private String seqOriginal;
     private String seqClean;
     private int seqCleanLength;
-    final double MASS_A = 135.128;
-    final double MASS_C = 111.103;
-    final double MASS_G = 151.128;
-    final double MASS_T = 125.107;
-    final double MASS_JUNK = 100.000;
+    private final double MASS_A = 135.128;
+    private final double MASS_C = 111.103;
+    private final double MASS_G = 151.128;
+    private final double MASS_T = 125.107;
+    private final double MASS_JUNK = 100.000;
+    private final String[] validEnd = {"TAA", "TAG", "TGA"};
 
     public DNA(String seq) {
         seqOriginal = seq;
         seqClean = removeJunk(seqOriginal);
         seqCleanLength = seqClean.length();
 
-        if(!validSeq()) {
+        if(!validSeq(seqClean)) {
             throw new IllegalArgumentException("Invalid DNA sequence");
         }
-
     }
 
     //@return true if DNA sequence is a protein, otherwise returns false
     public boolean isProtein() {
 
-        boolean starts = seqClean.substring(0,2).equals("ATG");
-        boolean ends = seqClean.substring(seqCleanLength-3, seqCleanLength-1).equals("TAA") || seqClean.substring(seqCleanLength-3, seqCleanLength-1).equals("TAG") || seqClean.substring(seqCleanLength-3, seqCleanLength-1).equals("TGA");
-        boolean containsCodons = seqCleanLength >= 5;
-        boolean containsCG = percentCG() >= 30.0;
-
-        if (starts && ends && containsCodons && containsCG) {
-            return true;
+        if (seqClean.startsWith("ATG")) {
+            if (seqCleanLength >= 5) {
+                if (percentCG() >= 30.0) {
+                    for (String codon : validEnd) {
+                        if (seqClean.endsWith(codon)) {
+                            return true;
+                        }
+                    }
+                }
+            }
         }
-
         return false;
     }
 
-
-    //@return double value of the mass of the DNA sequence, rounded to first decimal
+    //@return value of the mass of the DNA sequence, rounded to first decimal
     public double totalMass() {
         double junkMass = (seqOriginal.length() - seqCleanLength) * MASS_JUNK;
         double totalMass = junkMass;
@@ -67,40 +64,78 @@ public class DNA {
         return totalMass;
     }
 
-    //returns count of occurrence of nucleotide 'c' in DNA sequence
+    /*
+     @param c a valid nucleotide
+     @return count of occurrence of a nucleotide in DNA sequence
+    */
     public int nucleotideCount(char c) {
-        return 0;
-    }
+        int count = 0;
 
-    //returns a Set that contains all the distinct codons in DNA sequence;
-    public HashSet codonSet() {
-        return null;
-    }
+        if (c != 'A' && c != 'T' && c != 'C' && c != 'G') {
+            return count;
+        }
 
-    public String mutateCodon(String originalCodon, String newCodon) {
-        return seqClean;
-    }
-
-    //returns mass percentage of CG
-    public double percentCG() {
-        double percent;
-        double massCG = 0;
         for (int i = 0; i < seqCleanLength; i++) {
-            Character currentChar = seqClean.charAt(i);
-            if (currentChar.equals('C')) {
-                massCG += MASS_C;
-            }
-            else if (currentChar.equals('G')) {
-                massCG += MASS_G;
+            char currentChar = seqClean.charAt(i);
+            if (currentChar == c) {
+                count++;
             }
         }
 
+        return count;
+    }
+
+    //@return a Set that contains all the distinct codons in DNA sequence
+    public HashSet codonSet() {
+
+        HashSet<String> codons = new HashSet<String>();
+
+        for (int i = 0; i < seqCleanLength; i += 3) {
+            codons.add(seqClean.substring(i, i+3));
+        }
+
+        return codons;
+    }
+
+    /*
+     Changes all instances of a codon in the sequence to another codon
+     @param codonOrg the String of the codon you want to change
+     @param codonNew the String to change the old codon into
+           ^^ must be valid codons
+     */
+    public void mutateCodon(String codonOrg, String codonNew) {
+
+        String seqNew = "";
+
+        for (int i = 0; i < seqCleanLength; i += 3) {
+            if (seqClean.substring(i, i+3).equals(codonOrg)) {
+                seqNew += codonNew;
+            }
+            else {
+                seqNew += seqClean.substring(i, i+3);
+            }
+        }
+
+        String validCodon = removeJunk(codonNew);
+        if (validSeq(validCodon)) {
+            this.seqOriginal = seqNew;
+        }
+    }
+
+    //@return mass percentage of CG
+    public double percentCG() {
+        double percent;
+        double massCG = 0;
+
+        massCG = nucleotideCount('C') * MASS_C + nucleotideCount('G') * MASS_G;
+
         massCG = Math.round(massCG * 10.0) / 10.0;
         percent = massCG / this.totalMass() * 100.0;
+
         return percent;
     }
 
-    // returns original sequence of DNA as String
+    //@return original sequence of DNA
     public String sequence() {
         return this.seqOriginal;
     }
@@ -111,24 +146,32 @@ public class DNA {
      @return result sequence with junk characters removed
     */
     private String removeJunk(String seq) {
+
         String result = "";
+
         for (int i = 0; i < seq.length(); i++) {
-            Character currentChar = seq.charAt(i);
-            if (currentChar.equals('A') || currentChar.equals('T') || currentChar.equals('C') || currentChar.equals('G')) {
-                result = result + currentChar;
+            char currentChar = seq.charAt(i);
+            if (currentChar == 'A' || currentChar == 'T' || currentChar == 'C' || currentChar =='G') {
+                result += currentChar;
             }
         }
+
         return result;
     }
 
-    //checks if sequence is valid
-    private boolean validSeq() {
-        if (seqCleanLength % 3 == 0) {
+    /*
+     Checks to see if the DNA sequence is valid
+     @param seq a sequence of DNA
+     @return true if sequence is valid and false otherwise
+    */
+    private boolean validSeq(String seq) {
+
+        if (seq.length() % 3 == 0) {
             return true;
         }
         else{
             return false;
         }
-    }
 
+    }
 }
